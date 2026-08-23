d9284f885d0f069f004d22988d2104c70e7605b1
Vinicius Trovo <vinicius.martins@peers.com.br>
2026-08-22 22:49:05 -0300

    "fix-protect-operational-workflows-api"

--- src/modules/operational-workflows/operational-workflows.controller.ts
+++ src/modules/operational-workflows/operational-workflows.controller.ts
@@ -1,11 +1,9 @@
 'use strict';
 
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { Public } from '../../common/decorators/public.decorator.js';
 import { PreviewOperationalWorkflowDto } from './dto/preview-operational-workflow.dto.js';
 import { OperationalWorkflowsService } from './operational-workflows.service.js';
 
-@Public()
 @Controller('operations/workflows')
 export class OperationalWorkflowsController {
   constructor(private readonly workflows: OperationalWorkflowsService) {}
